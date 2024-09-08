1f94e7fb8221b33fec09093d87744dfb8184537b
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2024-09-08 13:38:27 +0530

    feat: homepage and footer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,11 +6,11 @@ import NavIcons from "./NavIcons";
 
 const Navbar = () => {
   return (
-    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
       {/* MOBILE */}
       <div className="h-full flex items-center justify-between md:hidden">
         <Link href="/" className="flex items-center gap-3">
-          <div className="text-2xl tracking-wide ">ECART</div>
+          <div className="text-2xl tracking-wide">ECART</div>
         </Link>
         <Menu />
       </div>
---
+++ src/components/ProductList.tsx
@@ -0,0 +1,127 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const ProductList = () => {
+  return (
+    <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+      <Link
+        href="/"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://cdn.pixabay.com/photo/2016/07/07/16/46/dice-1502706_640.jpg"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My Description</div>
+        <button className="rounded-2xl ring-1 ring-ecart text-ecart w-max py-2 px-4 text-xs hover:bg-ecart hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://cdn.pixabay.com/photo/2016/07/07/16/46/dice-1502706_640.jpg"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My Description</div>
+        <button className="rounded-2xl ring-1 ring-ecart text-ecart w-max py-2 px-4 text-xs hover:bg-ecart hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://cdn.pixabay.com/photo/2016/07/07/16/46/dice-1502706_640.jpg"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My Description</div>
+        <button className="rounded-2xl ring-1 ring-ecart text-ecart w-max py-2 px-4 text-xs hover:bg-ecart hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://cdn.pixabay.com/photo/2016/07/07/16/46/dice-1502706_640.jpg"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My Description</div>
+        <button className="rounded-2xl ring-1 ring-ecart text-ecart w-max py-2 px-4 text-xs hover:bg-ecart hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default ProductList;
