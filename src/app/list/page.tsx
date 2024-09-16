7fe9f1480483f6f5fe6a7e6b579f876c77573621
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2024-09-17 00:02:32 +0530

    feat: product list

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,8 +1,16 @@
 import Filter from "@/components/Filter";
 import ProductList from "@/components/ProductList";
+import { wixClientServer } from "@/lib/wixClientServer";
 import Image from "next/image";
+import { Suspense } from "react";
 
-const ListPage = () => {
+const ListPage = async ({ searchParams }: { searchParams: any }) => {
+  const wixClient = await wixClientServer();
+
+  const cat = await wixClient.collections.getCollectionBySlug(
+    searchParams.cat || "all-products"
+  );
+
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
       {/* CAMPAIGN */}
@@ -29,7 +37,14 @@ const ListPage = () => {
       <Filter />
       {/* PRODUCTS  */}
       <h1 className="mt-12 text-xl font-semibold">Shoes for you!</h1>
-      <ProductList></ProductList>
+      <Suspense fallback={"loading..."}>
+        <ProductList
+          categoryId={
+            cat.collection?._id || "00000000-000000-000000-000000000001"
+          }
+          searchParams={searchParams}
+        />
+      </Suspense>
     </div>
   );
 };
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -9,9 +9,11 @@ const PRODUCT_PER_PAGE = 20;
 const ProductList = async ({
   categoryId,
   limit,
+  searchParams,
 }: {
   categoryId: string;
   limit?: number;
+  searchParams?: any;
 }) => {
   const wixClient = await wixClientServer();
 
